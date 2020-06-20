7869b80062a33041321196be63e4c3e402d6b102
gubenites <gubenites99@gmail.com>
2020-06-20 17:31:16 -0300

    atualizado

--- 16-processos/teste.c
+++ 16-processos/teste.c
@@ -1,9 +1,23 @@
-#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/resource.h>
 
 int main(){
-  int variavel = 5;
-  int x = 0;
-  printf("%d",variavel);
+  pid_t pai, filho;
+
+  for(int i = 1 ; i <= 8 ; i++){
+    filho = fork();
+
+    if (filho == 0) {
+      pai = getpid();
+
+      printf("Eu sou o processo filho %d, meu pai é %d \n", filho,pai);
+      break
+    }else{
+      wait(NULL);
+    }
+  }
+  return 0;
 }
