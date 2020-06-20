044c41c195dbefa396ce8295cae74767c606a360
gubenites <gubenites99@gmail.com>
2020-06-20 16:33:03 -0300

    atualizado

--- 16-processos/exemplo1-fork.c
+++ 16-processos/exemplo1-fork.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main() {
+int main(){
     pid_t pai, filho;
     int variavel = 5;
 
@@ -12,13 +12,11 @@ int main() {
         pai = getppid();
         filho = getpid();
         variavel *= 2;
-        printf("eu sou o processo filho %d, meu pai é %d\nvariavel %d\n",
-            filho, pai, variavel);
+        printf("eu sou o processo filho %d, meu pai é %d\nvariavel %d\n",filho, pai, variavel);
     } else {
         // processo pai aqui!
         pai = getpid();
-        printf("eu sou o processo pai %d, meu filho é %d\nvariavel %d\n",
-            pai, filho, variavel);
+        printf("eu sou o processo pai %d, meu filho é %d\nvariavel %d\n",pai, filho, variavel);
     }
     return 0;
 }
