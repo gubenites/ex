9c014b1807b2754f5b99015216b16d7decb81999
gubenites <gubenites99@gmail.com>
2020-06-20 20:03:12 -0300

    atualizado

--- 17-exec/envia_arg.c
+++ 17-exec/envia_arg.c
@@ -6,7 +6,7 @@
 
 
 int main(){
-  int num;
+  int num, ret_eh_par, st;
   char prog[] = "./sum";
   char *args[3];
   char num_send[20];
@@ -19,6 +19,18 @@ int main(){
       args[1] = num_send;
       args[2] = NULL;
       execvp(prog, args);
+    }else {
+      wait(&st);
+      if (WIFEXITED(st)){
+        ret_eh_par = (char) WEXITSTATUS(st);
+        if (ret_eh_par == 0) {
+          printf("É impar\n");
+        } else if(ret_eh_par == 1){
+          printf("É par\n");
+        }
+      }
     }
-  }
+  } while (ret_eh_par != -1)
+
+  return 0;
 }
