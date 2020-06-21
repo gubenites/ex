c45d58137eaf703c20320be4d0cd3d76e5179485
gubenites <gubenites99@gmail.com>
2020-06-20 22:08:30 -0300

    atualizado

--- 18-sinais-I/parte1.c
+++ 18-sinais-I/parte1.c
@@ -1,20 +1,30 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <signal.h>
 
 int main() {
-    if (fork() == 0) {
+    pid_t filho;
+    filho = fork();
+    if (filho == 0) {
         printf("%d\n", getpid());
+        return 5;
         while(1){
 
         }
-    } else {
-        int st;
-        wait(&st);
-        if (WIFEXITED(st)) printf("return: %d\n", WEXITSTATUS(st));
-        if (WIFSIGNALED(st)) printf("signal: %s\n", strsignal(WTERMSIG(st)));
     }
+    sleep(10);
+    int st;
+    pid_t filho_acabou = waitpid(filho,&st,WHONANG);
+    if (filho_acabou == 0) {
+      kill(filho, SIGKILL);
+      wait(&st);
+    }
+
+    if (WIFEXITED(st)) printf("return: %d\n", WEXITSTATUS(st));
+    if (WIFSIGNALED(st)) printf("signal: %s\n", strsignal(WTERMSIG(st)));
+
+    return 0;
 }
