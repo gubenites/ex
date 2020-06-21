1deb76678bf66aff9b9fb385c4b52fe75dde5cfd
gubenites <gubenites99@gmail.com>
2020-06-21 00:46:11 -0300

    atualizado

--- 19-sinais-II/sinal1.c
+++ 19-sinais-II/sinal1.c
@@ -2,28 +2,50 @@
 #include <unistd.h>
 #include <signal.h>
 
-int counter = 0;
+int status = 0;
 
-void sig_handler(int num) {
-    printf("Chamou Ctrl+C\n");
+void operacao_lenta() {
+    sleep(10);
+}
 
-    counter += 1;
-    if (counter == 3) {
-      exit(0);
-    }
+void sigint_handler(int num) {
+  status += 1;
+  printf("Chamou Ctrl+C; status=%d\n", status);
+  operacao_lenta();
+  printf("SIGINT: Vou usar status agora! status=%d\n", status)
+}
+
+void sigterm_handler(int num){
+  status += 1;
+  printf("Chamou SIGTERM; status=%d\n", status);
+  operacao_lenta();
+  printf("SIGTERM: Vou usar status agora! status=%d\n", status)
 }
 
 int main() {
     /* TODO: registre a função sig_handler
      * como handler do sinal SIGINT
      */
-    printf("Meu pid: %d\n", getpid());
     struct sigaction s;
-    s.sa_handler = sig_handler;
+    printf("Meu pid: %d\n", getpid());
+    s.sa_handler = sigint_handler;
     sigemptyset(&s.sa_mask);
-    s.sa_flags = 0;
 
+    sigaddset(&s.sa_mask, SIGTERM);
+
+    s.sa_flags = 0;
     sigaction(SIGINT, &s, NULL);
+
+
+    /* TODO: registar SIGTERM aqui. */
+    struct sigaction sigterm;
+    sigterm.sa_handler = sigterm_handler;
+    sigemptyset(&sigterm.sa_mask);
+
+    sigaddset(&sigterm.sa_mask, SIGINT);
+
+    sigterm.sa_flags = 0;
+    sigaction(SIGTERM, &sigterm, NULL);
 
     while(1) {
       sleep(1);
